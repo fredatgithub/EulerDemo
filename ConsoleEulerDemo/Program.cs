e322577f022a905fdb0387f72454d601582916e4
Freddy Juhel <domaine75@gmail.com>
2015-10-13 14:13:10 +0200

    Initial commit

---
+++ ConsoleEulerDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleEulerDemo
+{
+  class Program
+  {
+    static void Main()
+    {
+      foreach (var partialSum in GetEulerPartialSum(1000000))
+      {
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(partialSum);
+      }
+      Console.ReadLine();
+    }
+
+    private static IEnumerable<double> GetEulerPartialSum(int n)
+    {
+      double sum = 0.0;
+      for (int i = 1; i <= n; i++)
+      {
+        sum += 1.0 / Math.Pow(i, 2);
+        yield return sum;
+      }
+    }
+  }
+}
